4d1a7288ffdce1557f1084dfecab1f9343b486a7
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2022-05-01 16:55:18 -0700

    examples added to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -78,21 +78,21 @@ impl Parser {
     /// ```
     /// use std::env; //allows access to the process's environment
     /// 
-    /// use clia;
+    /// use clia::{option_args, parameter_args, Parser};
     /// 
     /// //collect cli arguments
     /// let args: Vec<String> = env::args().collect();
     /// 
     /// //define valid options
-    /// let valid_options: Vec<clia::option_args::ClOption> = Vec::new();
+    /// let valid_options: Vec<option_args::ClOption> = Vec::new();
     /// //...
     /// 
     /// //define expected parameters
-    /// let expected_parameters: Vec<clia::parameter_args::ClParameter> = Vec::new();
+    /// let expected_parameters: Vec<parameter_args::ClParameter> = Vec::new();
     /// //..
     /// 
     /// //create a new parser
-    /// let parser = clia::Parser::new(&args, &valid_options, &parameter_args);
+    /// let parser = Parser::new(&args, &valid_options, &expected_parameters);
     /// ```
     pub fn new(args: &[String], valid_options: &[option_args::ClOption], expected_parameters: &[parameter_args::ClParameter]) -> Result<Parser, Box<dyn Error>> {
         //DATA
@@ -140,7 +140,35 @@ impl Parser {
     /// 
     /// # Examples
     /// ```
+    /// use std::env;
     /// 
+    /// use clia::{option_args, parameter_args, Parser};
+    /// 
+    /// //collect cli arguments
+    /// //NOTE: in practice, you'd do it like this ... but for the sake of this example, we'll be creating our own vector
+    /// //let args: Vec<String> = env::args().collect();
+    /// let args: Vec<String> = vec![String::from("-0")];
+    /// 
+    /// //define valid options
+    /// let valid_options: Vec<option_args::ClOption> = Vec::new();
+    /// //...
+    /// 
+    /// //define expected parameters
+    /// let expected_parameters: Vec<parameter_args::ClParameter> = Vec::new();
+    /// //..
+    /// 
+    /// //call parser, show help it if fails
+    /// let arg_parser;
+    /// match Parser::new(&args, &valid_options, &expected_parameters) {
+    ///     Ok(arg_par) => arg_parser = arg_par,
+    ///     Err(e) => {
+    ///         println!("{}", Parser::help("foo.exe", "by Anthony Rubick", "Just here as an example of things you can do", &valid_options, &expected_parameters)); 
+    ///         //in practice, you'd want to panic here, like so:
+    ///         //`panic!("{}", e); //print any errors that occur`
+    ///         //but for the sake of this example, the error will just be printed
+    ///         println!("{}",e);
+    ///     },
+    /// }
     /// ```
     pub fn help(title: &str, author: &str, program_description: &str, valid_options: &[option_args::ClOption], expected_parameters: &[parameter_args::ClParameter]) -> String {
         format!("{}\n{}\n\n{}\n\nUSAGE: {} [OPTIONS]... {}\n\nOPTIONS:\n{}\nPARAMETER ARGUMENTS:\n{}",
@@ -176,30 +204,102 @@ impl Parser {
 
     //getter methods
     /// get a reference to `valid_options`
-    /// # Example 
+    /// # Examples 
     /// ```
+    /// use clia::{option_args::{ClOptionInfo, ClOption}, parameter_args, Parser};
     /// 
+    /// let args: Vec<String> = vec![String::from("path/to/executable/"), String::from("-h")];
+    /// 
+    /// //define valid options
+    /// let valid_options: Vec<ClOption> = vec![
+    ///     ClOption::new_flag(&ClOptionInfo::new("-r", "--recursive", "Search through subdirectories").unwrap()),
+    ///     ClOption::new_flag_list( &ClOptionInfo::new("-f", "--filter", "Comma separated list of extensions, will only count lines of files with these extensions").unwrap(), "EXTENSIONS"),
+    ///     ClOption::new_flag_list( &ClOptionInfo::new("-F", "--format", "Format the output in a list, valid formats are: DEFAULT, BULLET, MARKDOWN, and NUMERIC").unwrap(), "FORMAT"),
+    ///     ClOption::new_flag(&ClOptionInfo::new("-h", "--help", "Show help").unwrap()),
+    /// ];
+    /// 
+    /// //define expected parameters
+    /// let expected_parameters: Vec<parameter_args::ClParameter> = Vec::new();
+    /// //..
+    /// 
+    /// //create a new parser
+    /// let parser = Parser::new(&args, &valid_options, &expected_parameters).unwrap();
+    /// 
+    /// assert_eq!(parser.get_valid_options(), &valid_options);
     /// ```
     pub fn get_valid_options(&self) -> &Vec<option_args::ClOption> {&self.valid_options}
 
     /// get a reference to `expected_parameters`
-    /// # Example 
+    /// # Examples 
     /// ```
+    /// use clia::{option_args::{ClOptionInfo, ClOption}, parameter_args::ClParameter, Parser};
     /// 
+    /// let args: Vec<String> = vec![String::from("path/to/executable/"), String::from("path/to/search"), String::from("thing to search for")];
+    /// 
+    /// //define valid options
+    /// let valid_options: Vec<ClOption> = Vec::new();
+    /// //..
+    /// 
+    /// //define expected parameters
+    /// let expected_parameters: Vec<ClParameter> = vec![
+    ///     ClParameter::new("PATH", "Path to search in"),
+    ///     ClParameter::new("QUERY", "String to search for, all the stuff after the path wrap in \"'s if it contains spaces"),
+    /// ];
+    /// 
+    /// //create a new parser
+    /// let parser = Parser::new(&args, &valid_options, &expected_parameters).unwrap();
+    /// 
+    /// assert_eq!(parser.get_expected_parameters(), &expected_parameters);
     /// ```
     pub fn get_expected_parameters(&self) -> &Vec<parameter_args::ClParameter> {&self.expected_parameters}
 
     /// get a reference to `option_arguments_found`
-    /// # Example 
+    /// # Examples 
     /// ```
+    /// use clia::{option_args::{ClOptionInfo, ClOption}, parameter_args, Parser};
     /// 
+    /// let args: Vec<String> = vec![String::from("path/to/executable/"), String::from("-h")];
+    /// 
+    /// //define valid options
+    /// let valid_options: Vec<ClOption> = vec![
+    ///     ClOption::new_flag(&ClOptionInfo::new("-r", "--recursive", "Search through subdirectories").unwrap()),
+    ///     ClOption::new_flag_list( &ClOptionInfo::new("-f", "--filter", "Comma separated list of extensions, will only count lines of files with these extensions").unwrap(), "EXTENSIONS"),
+    ///     ClOption::new_flag_list( &ClOptionInfo::new("-F", "--format", "Format the output in a list, valid formats are: DEFAULT, BULLET, MARKDOWN, and NUMERIC").unwrap(), "FORMAT"),
+    ///     ClOption::new_flag(&ClOptionInfo::new("-h", "--help", "Show help").unwrap()),
+    /// ];
+    /// 
+    /// //define expected parameters
+    /// let expected_parameters: Vec<parameter_args::ClParameter> = Vec::new();
+    /// //..
+    /// 
+    /// //create a new parser
+    /// let parser = Parser::new(&args, &valid_options, &expected_parameters).unwrap();
+    /// 
+    /// assert_eq!(parser.get_option_arguments_found().iter().filter(|opt| opt.get_present()).collect::<Vec<&ClOption>>().get(0).unwrap().get_info(), &ClOptionInfo::new("-h", "--help", "Show help").unwrap());
     /// ```
     pub fn get_option_arguments_found(&self) -> &Vec<option_args::ClOption> {&self.option_arguments_found}
 
     /// get a reference to `parameter_arguments_found`
-    /// # Example 
+    /// # Examples 
     /// ```
+    /// use clia::{option_args, parameter_args::ClParameter, Parser};
     /// 
+    /// let args: Vec<String> = vec![String::from("path/to/executable/"), String::from("path/to/search"), String::from("thing to search for")];
+    /// 
+    /// //define valid options
+    /// let valid_options: Vec<option_args::ClOption> = Vec::new();
+    /// //..
+    /// 
+    /// //define expected parameters
+    /// let expected_parameters: Vec<ClParameter> = vec![
+    ///     ClParameter::new("PATH", "Path to search in"),
+    ///     ClParameter::new("QUERY", "String to search for, all the stuff after the path wrap in \"'s if it contains spaces"),
+    /// ];
+    /// 
+    /// //create a new parser
+    /// let parser = Parser::new(&args, &valid_options, &expected_parameters).unwrap();
+    /// 
+    /// assert_eq!(parser.get_parameter_arguments_found().iter().map(|param| param.get_data()).collect::<Vec<&str>>(), vec!["path/to/search", "thing to search for"]);
     /// ```
     pub fn get_parameter_arguments_found(&self) -> &Vec<parameter_args::ClParameter> {&self.parameter_arguments_found}
     
